4e2ffa9a0db8ac1ae1f632ca6589d4fb88bd31d1
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-10 15:53:02 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,7 @@ int main(void)
 printf("Size of char %d byte(s)n", sizeof(char));
 printf("Size of float %d byte(s)n", sizeof(float));
 printf("Size of int %d byte(s)n", sizeof(int));
+printf("Size of long int %d byte(s)n", sizeof(long int));
+printf("Size of long long int %d byte(s)n", sizeof(long long int));
 return (0);
 }
