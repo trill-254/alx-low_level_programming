7c67a0c4be71e3b025592691102e334f0c18a8d7
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-14 14:30:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 28; i < 38; i++)
+	{
+		putchar(i);
+	}
+	for (i = 103; i < 113; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
