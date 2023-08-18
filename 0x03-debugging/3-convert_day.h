45a27c62aa453c10b531f256c97bbf12037354dd
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-18 14:37:54 -0700

    3-print_remaining_days.c, main.h

---
+++ 0x03-debugging/3-convert_day.h
@@ -0,0 +1,6 @@
+#ifndef CONVERT_DAY_H
+#define CONVERT_DAY_H
+
+int convert_day(int month, int day);
+
+#endif
---
+++ 0x03-debugging/3-print_remaining_days.h
@@ -0,0 +1,6 @@
+#ifndef PRINT_REMAINING_DAYS_H
+#define PRINT_REMAINING_DAYS_H
+
+void print_remaining_days(int month, int day, int year);
+
+#endif
