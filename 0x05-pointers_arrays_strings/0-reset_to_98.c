f68c4d55eec16abc7247c6d98ccdbe50b0367687
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-21 05:47:40 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ * the value it points to 9
+ *
+ * @n:  The pointer
+ *
+ * Return:nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
