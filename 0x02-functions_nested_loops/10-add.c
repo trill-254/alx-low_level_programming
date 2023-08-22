6e96828e17599b6f142a003cc07e5a78052320e5
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-22 13:53:06 -0700

    100-main.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "math.h"
+
+
+
+/**
+ * add - Adds two integers and returns the result
+ * @n1: The first integer to be computed
+ * @n2: The second integer to be computed
+ * Retuen: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+        return (n1 + n2);
+}
