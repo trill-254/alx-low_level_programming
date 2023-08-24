e386cd87129e1769e8f20ec38c6c56a4bb78ac9f
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-24 11:46:15 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies up to n bytes of one string to another
+ * @dest: The destination string
+ * @src: The source string to copy from
+ * @n: The maximum number of bytes to copy
+ *
+ * Return: A pointer to the resulting string @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+
+return (dest);
+}
