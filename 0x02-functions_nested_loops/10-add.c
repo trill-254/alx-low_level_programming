cae04c3477b02ab6e9c53396e08593c165db3e73
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-24 14:53:50 -0700

    Werror

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,12 +6,12 @@
  * add - Adds two integers and returns the result
  * @n1: The first integer to be computed
  * @n2: The second integer to be computed
- * Retuen: Integer value
+ * Return: Integer value
  */
 
 int add(int n1, int n2)
 
 {
 
-        return (n1 + n2);
+return (n1 + n2);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int print_last_digit(int);
 int add(int, int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-void jack_bauser(void);
+void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
 void print_times_table(int n);
