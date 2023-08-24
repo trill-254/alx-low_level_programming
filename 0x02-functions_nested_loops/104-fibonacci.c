0c83e83cfade968333788f67046207187005bf49
JUNE NGUNJIRI <mainajane316@gmail.com>
2023-08-24 15:13:56 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,18 +5,19 @@ void print_fibonacci(int n);
 int main(void)
 {
     print_fibonacci(98);
-    return (0);
+    return 0;
 }
 
 void print_fibonacci(int n)
 {
     int a = 1, b = 2;
+    int next;
 
     printf("%d, %d", a, b);
 
     for (int i = 3; i <= n; ++i)
     {
-        int next = a + b;
+        next = a + b;
         printf(", %d", next);
 
         a = b;
